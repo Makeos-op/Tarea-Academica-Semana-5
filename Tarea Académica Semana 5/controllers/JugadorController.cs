9011bb037147c2988dee8a8884306b455df179e3
Sebastian Medina <smedinamayo@gmail.com>
2025-10-01 00:53:47 -0500

    Controladores & Comentarios

---
+++ Tarea Académica Semana 5/controllers/JugadorController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Académica_Semana_5.controllers
+{
+    internal class JugadorController
+    {
+    }
+}
--- Tarea Académica Semana 5/entities/Videojuegos.cs
+++ Tarea Académica Semana 5/entities/Videojuegos.cs
@@ -8,12 +8,14 @@ namespace Tarea_Académica_Semana_5.entities
 {
     internal class Videojuegos
     {
+        // Constructor principal que inicializa la lista de jugadores (junto con los otros datos)
         public Videojuegos()
         {
             Jugadores = new List<Jugador>();
         }
         public string Codigo { get; set; }
         public string Nombre { get; set; }
+        // Lista de jugadores
         public List<Jugador> Jugadores { get; set; }
     }
 }
