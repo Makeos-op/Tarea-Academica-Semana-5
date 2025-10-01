3368a1d7e782d624396b9bab336534fedf4409c9
Sebastian Medina <smedinamayo@gmail.com>
2025-09-30 22:52:52 -0500

    Creacion de las clases

---
+++ Tarea Académica Semana 5/entities/Jugador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Académica_Semana_5.entities
+{
+    internal class Jugador
+    {
+        public int DNI { get; set; }
+        public string Alias { set; get; }
+    }
+}
---
+++ Tarea Académica Semana 5/entities/Videojuegos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Académica_Semana_5.entities
+{
+    internal class Videojuegos
+    {
+        public Videojuegos()
+        {
+            Jugadores = new List<Jugador>();
+        }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public List<Jugador> Jugadores { get; set; }
+    }
+}
